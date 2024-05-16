6f5698691c1fa61348e4715ca405befec0eef446
Manu-Andaluz <manupeandaluz@gmail.com>
2024-05-16 17:46:43 -0300

    checking if the user is admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,18 +1,40 @@
+"use client";
 import * as React from "react";
 import Container from "@mui/material/Container";
 import NavBar from "@/components/admin/navbar";
+import { getUser } from "@/lib/auth/client";
+import { UserToken } from "@/types/user";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
+  const [isChecking, setIsChecking] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const token = getUser() as UserToken;
+
+    if (token.role === "admin") {
+      setIsChecking(false);
+      return;
+    }
+
+    return;
+  }, []);
+
   return (
-    <main>
-      <NavBar />
-      <Container maxWidth="xl" sx={{ py: "24px" }}>
-        {children}
-      </Container>
-    </main>
+    <>
+      {isChecking ? (
+        ""
+      ) : (
+        <main>
+          <NavBar />
+          <Container maxWidth="xl" sx={{ py: "24px" }}>
+            {children}
+          </Container>
+        </main>
+      )}{" "}
+    </>
   );
 }
